e1419d73a2b8ccf7bda1d3603ad887bac0b33bc7
Clement Laurenti <clement.laurenti@hotmail.fr>
2020-04-11 20:37:32 +0200

    Speed Scene Loader added

---
+++ ProjetCK/Assets/Project/Scripts/Editor/SpeedSceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SpeedSceneLoader : EditorWindow
+{
+    private string sceneName;
+
+    [MenuItem("Window/SceneLoader")]
+    public static void ShowWindow()
+    {
+        GetWindow<SpeedSceneLoader>("SpeedSceneLoader");
+    }
+
+    private void OnGUI() 
+    {
+        EditorGUILayout.BeginVertical();
+        
+        for(int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            string path = EditorBuildSettings.scenes[i].path;
+            string[] pathSplit = path.Split('/','.');
+
+            sceneName = pathSplit[pathSplit.Length - 2];
+            
+            if (GUILayout.Button(sceneName))
+            {
+                EditorSceneManager.OpenScene(path);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+}
